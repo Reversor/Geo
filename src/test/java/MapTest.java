36551a329a619be3c0035693ac277e0f2897439e
Rost Zhiharev <revers914@gmail.com>
2018-02-26 09:51:54 +0400

    update test methods

--- src/test/java/MapTest.java
+++ src/test/java/MapTest.java
@@ -1,27 +1,27 @@
 import entity.Person;
+import entity.Person.Gender;
+import java.util.Map;
+import java.util.Set;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.MyMap;
 import util.PersonGenerator;
 
-import java.util.Map;
-import java.util.Set;
-
 public class MapTest extends Assert {
+
     private final Logger LOG = LoggerFactory.getLogger(this.getClass().getName());
     private Map<Integer, Person> map;
-    private Person golovachLena = new Person("Лена", "Головач", Person.Gender.MALE, 20 );
+    private Person golovachLena = new Person("Лена", "Головач", Person.Gender.MALE, 20);
     private Person pilnikYana = new Person("Яна", "Пильник", Person.Gender.FEMALE, 40);
 
     @Before
     public void init() {
         map = new MyMap<>();
         PersonGenerator personGenerator = new PersonGenerator();
-        for (int i = 0; i < 15; i++ ) {
+        for (int i = 0; i < 15; i++) {
             map.put(i, personGenerator.getNewPerson());
         }
     }
@@ -49,50 +49,51 @@ public class MapTest extends Assert {
         assertEquals(length - 2, map.size());
     }
 
-    @Test(timeout = 0)
-    @Ignore
+    @Test(timeout = 1_000)
     public void forEach() {
-//        map.entrySet().spliterator().forEachRemaining(System.out::println);
+        map.entrySet().spliterator().forEachRemaining((entry) -> {
+        });
         map.entrySet().parallelStream()
                 .map(entry -> entry.getKey() + " " + entry.getValue())
-                .forEach(System.out::println);
+                .forEach((entry) -> {
+                });
         assertFalse(
                 map.entrySet().parallelStream()
                         .filter(entry -> entry.getValue().getGender() == Person.Gender.MALE)
-                        .anyMatch(entry -> entry.getValue().equals("ответ"))
+                        .anyMatch(entry -> entry.getValue().getGender() == Gender.FEMALE)
         );
         map.values().parallelStream()
-                .filter(value -> value.equals("ответ"))
-                .forEach((e) -> {
+                .filter(value -> value.getAge() > 50)
+                .forEach((value) -> {
+                    assertTrue(value.getAge() > 50);
                 });
         map.keySet().parallelStream()
                 .filter(key -> key % 2 == 0)
-                .forEach((e) -> {
+                .forEach((k) -> {
+                    assertFalse(k % 2 == 1);
                 });
     }
 
     @Test
-    @Ignore
     public void keySet() {
+        int size = map.size();
         assertNotNull(map.keySet());
         map.keySet().remove(2);
+        assertEquals(size - 1, map.size());
     }
 
     @Test
-    @Ignore
     public void values() {
         map.put(6, golovachLena);
         assertTrue(map.values().remove(golovachLena));
     }
 
     @Test
-    @Ignore
     public void entrySet() {
         assertNotNull(map.entrySet());
         Set<Map.Entry<Integer, Person>> set = map.entrySet();
         assertTrue(set.remove(1));
         assertTrue(set.size() == map.size());
-        System.out.println(set);
     }
 
 }
