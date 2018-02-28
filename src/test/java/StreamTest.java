179060fd9a4e067e10f63cfd2b15104327922716
Rost Zhiharev <revers914@gmail.com>
2018-02-28 11:13:35 +0400

    tested

--- src/test/java/StreamTest.java
+++ src/test/java/StreamTest.java
@@ -1,3 +1,7 @@
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+
 import entity.Person;
 import entity.Person.Gender;
 import java.util.Comparator;
@@ -15,21 +19,13 @@ import util.PersonGenerator;
 public class StreamTest extends Assert {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
-    private long avgTime;
-    private int testCount;
-    private int size;
     private Map<Integer, Person> myMap;
-    private PersonGenerator personGenerator;
 
     @Before
     public void init() {
-//        size = 5_000_000;
-        size = 20;
-        testCount = 1;
-        avgTime = 0;
-        personGenerator = new PersonGenerator();
+        int size = 5_000_000;
+        PersonGenerator personGenerator = new PersonGenerator();
         myMap = new MyMap<>();
-//        myMap = new HashMap<>();
         for (int i = 0; i < size; i++) {
             myMap.put(i, personGenerator.getNewPerson());
         }
@@ -38,10 +34,9 @@ public class StreamTest extends Assert {
     @Test
     public void mihailCount() {
         logger.info(Long.toString(myMap.values()
-                        .parallelStream()
-//                .stream()
-                        .filter(v -> v.getFirstName().equals("Mihail"))
-                        .count())
+                .parallelStream()
+                .filter(v -> v.getFirstName().equals("Mihail"))
+                .count())
         );
     }
 
@@ -68,13 +63,13 @@ public class StreamTest extends Assert {
     public void summaryTable() {
         logger.info("summary test started");
         Map<Gender, Map<Integer, Long>> summary = myMap.values().stream()
-                .collect(Collectors.groupingBy(Person::getGender,
-                        Collectors.groupingBy(Person::getAge, Collectors.counting())));
+                .collect(groupingBy(Person::getGender, groupingBy(Person::getAge, counting())));
         System.out.println(summary);
         long count = summary.values()
-                .stream().flatMap(map -> map.values().stream())
-                .mapToLong(v -> v)
-                .sum();
+                .stream()
+                .flatMap(map -> map.values().stream())
+                .reduce((a, v) -> a += v)
+                .orElse(0L);
         System.out.println(count);
     }
 
@@ -93,8 +88,7 @@ public class StreamTest extends Assert {
                 .sorted()
                 .skip((myMap.size() % 2) == 0
                         ? myMap.size() / 2 - 1
-                        : myMap.size() / 2 + 1
-                )
+                        : myMap.size() / 2 + 1)
                 .limit((myMap.size() % 2) == 0 ? 2 : 1)
                 .peek(System.out::println)
                 .average().getAsDouble();
@@ -105,28 +99,35 @@ public class StreamTest extends Assert {
     @Test
     public void ageSpreading() {
         System.out.println(myMap.values().stream()
-                .collect(Collectors.groupingBy(Person::getAge, Collectors.counting())));
+                .collect(Collectors.groupingBy(Person::getAge, counting())));
     }
 
     @Test
     public void percentil() {
         int percent = 25;
-        /*double median = myMap.values().stream()
-                .mapToInt(Person::getAge)
+        Map<Integer, Long> map = myMap.values().stream()
+                .collect(Collectors.groupingBy(Person::getAge, counting()));
+        double percentil = map.values().stream()
+                .mapToLong(Long::longValue)
                 .sorted()
-                .skip((myMap.size() % 2) == 0
-                        ? myMap.size() / 2 - 1
-                        : myMap.size() / 2 + 1
-                )
-                .limit((myMap.size() % 2) == 0 ? 2 : 1)
+                .skip(map.size() % (100 / percent) == 0
+                        ? (map.size() * percent / 100) - 1
+                        : (map.size() * percent / 100) + 1)
+                .limit(map.size() % (100 / percent) == 0 ? 2 : 1)
                 .peek(System.out::println)
-                .average().getAsDouble();*/
-        /*double percentil = myMap.values().stream()
-                .mapToInt(Person::getAge)
-                .sorted()
-                .skip((myMap.size() * percent / 100) - 1)
-                .limit(1)
-                .average().getAsDouble();*/
+                .average()
+                .getAsDouble();
         System.out.println(percentil);
+    }
+
+    @Test
+    public void percentrank() {
+        Map<Integer, Double> percentrank = myMap.values().stream()
+                .collect(Collectors.groupingBy(Person::getAge,
+                        collectingAndThen(counting(), e -> ((double) e / myMap.size()) * 100)));
+
+        System.out.println(percentrank);
+        System.out.println(percentrank.values()
+                .stream().reduce(0d, (a, v) -> a += v).byteValue());
     }
 }
